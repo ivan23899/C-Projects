5fa157275fce9be835abfb6e968bba7f4b85555d
Ruddy Ivan Claros Fernandez <na-vi-89@hotmail.com>
2021-10-10 15:00:14 -0400

    UsoDll

---
+++ OrdenarNumeros/OrdenadorNumerico.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OrdenarNumeros
+{
+    public class OrdenadorNumerico
+    {
+        public List<int> ordenarDescendente(List<int> listanumeros)
+        {
+            List<int> ordenados = listanumeros;
+            ordenados.Sort((a,b)=>b.CompareTo(a));
+            return ordenados;
+        }
+        public List<int> ordenarAscendente(List<int> listanumeros)
+        {
+            List<int> ordenados = listanumeros;
+            ordenados.Sort((a, b) => a.CompareTo(b));
+            return ordenados;
+        }
+    }
+}
---
+++ UsoDll/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using OrdenarNumeros;
+
+namespace UsoDll
+{
+
+    class Program
+    {
+        public static void Mostrar(List<int> lista)
+        {
+            for (int i=0;i<=lista.Count-1;i++)
+            {
+                Console.WriteLine(lista[i].ToString());
+            }
+        }
+        static void Main(string[] args)
+        {
+            OrdenadorNumerico objOrdenador = new OrdenadorNumerico();
+            List<int> listaDesordenada = new List<int>();
+            for (int i =1;i<=10;i++)
+            {
+                Random r = new();
+                listaDesordenada.Add(r.Next(10,101));
+            }
+            Console.WriteLine("Lista desordenada: ");
+            Console.WriteLine();
+            Mostrar(listaDesordenada);    
+            Console.WriteLine("Lista ordenada Descendentemente: ");
+            Console.WriteLine();
+            listaDesordenada = objOrdenador.ordenarDescendente(listaDesordenada);
+            Mostrar(listaDesordenada);
+
+            Console.WriteLine("Lista ordenada Ascendentemente: ");
+            Console.WriteLine();
+            listaDesordenada = objOrdenador.ordenarAscendente(listaDesordenada);
+            Mostrar(listaDesordenada);
+        }
+    }
+}
