a3b546f359d2648881c9a0353a83eca327d7a457
CLAROS <CLAROS@ASUS-STRIX-ROG->
2021-10-10 11:15:36 -0400

    Agregar archivos de proyecto.

---
+++ QrCode/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace QrCode
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.picQr = new System.Windows.Forms.PictureBox();
+            this.txtMensaje = new System.Windows.Forms.TextBox();
+            this.btnGenerar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.picQr)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // picQr
+            // 
+            this.picQr.Location = new System.Drawing.Point(298, 27);
+            this.picQr.Name = "picQr";
+            this.picQr.Size = new System.Drawing.Size(188, 180);
+            this.picQr.TabIndex = 0;
+            this.picQr.TabStop = false;
+            // 
+            // txtMensaje
+            // 
+            this.txtMensaje.Location = new System.Drawing.Point(257, 233);
+            this.txtMensaje.Name = "txtMensaje";
+            this.txtMensaje.Size = new System.Drawing.Size(273, 27);
+            this.txtMensaje.TabIndex = 1;
+            // 
+            // btnGenerar
+            // 
+            this.btnGenerar.Location = new System.Drawing.Point(321, 276);
+            this.btnGenerar.Name = "btnGenerar";
+            this.btnGenerar.Size = new System.Drawing.Size(139, 37);
+            this.btnGenerar.TabIndex = 2;
+            this.btnGenerar.Text = "Generar QR";
+            this.btnGenerar.UseVisualStyleBackColor = true;
+            this.btnGenerar.Click += new System.EventHandler(this.btnGenerar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnGenerar);
+            this.Controls.Add(this.txtMensaje);
+            this.Controls.Add(this.picQr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.picQr)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox picQr;
+        private System.Windows.Forms.TextBox txtMensaje;
+        private System.Windows.Forms.Button btnGenerar;
+    }
+}
+
---
+++ QrCode/Form1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using ZXing;
+using ZXing.Common;
+
+namespace QrCode
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnGenerar_Click(object sender, EventArgs e)
+        {
+            BarcodeWriter barcodewriter = new BarcodeWriter();
+            barcodewriter.Format = BarcodeFormat.QR_CODE;
+            Bitmap bitmap = barcodewriter.Write(txtMensaje.Text);
+            picQr.Image = bitmap;
+
+            LuminanceSource source = new BitmapLuminanceSource(bitmap);
+            BinaryBitmap binaryBitmap = new BinaryBitmap(new HybridBinarizer(source));
+            Result result = new MultiFormatReader().decode(binaryBitmap);
+            MessageBox.Show(result.Text);
+        }
+    }
+}
