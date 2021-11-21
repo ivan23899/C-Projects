08e3a98a1f1dd0326c44a9ed29efe44117657742
Ruddy Ivan Claros Fernandez <na-vi-89@hotmail.com>
2021-11-20 21:10:35 -0400

    CONVERT HEX TO TEXT / TEXT TO HEX

---
+++ ConvertHexToText/ConvertHexToText/Form1.Designer.cs
@@ -0,0 +1,135 @@
+﻿
+namespace ConvertHexToText
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.HexT = new System.Windows.Forms.Button();
+            this.TexH = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // HexT
+            // 
+            this.HexT.Location = new System.Drawing.Point(21, 222);
+            this.HexT.Name = "HexT";
+            this.HexT.Size = new System.Drawing.Size(150, 31);
+            this.HexT.TabIndex = 1;
+            this.HexT.Text = "HEX To TEXT";
+            this.HexT.UseVisualStyleBackColor = true;
+            this.HexT.Click += new System.EventHandler(this.HexT_Click);
+            // 
+            // TexH
+            // 
+            this.TexH.Location = new System.Drawing.Point(21, 444);
+            this.TexH.Name = "TexH";
+            this.TexH.Size = new System.Drawing.Size(150, 40);
+            this.TexH.TabIndex = 2;
+            this.TexH.Text = "TEXT To HEX";
+            this.TexH.UseVisualStyleBackColor = true;
+            this.TexH.Click += new System.EventHandler(this.TexH_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(21, 50);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(588, 166);
+            this.textBox1.TabIndex = 3;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(21, 272);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(588, 166);
+            this.textBox2.TabIndex = 4;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(697, 450);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "CLEAR";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point);
+            this.label1.Location = new System.Drawing.Point(204, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(424, 31);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "CONVERT HEX TO TEXT / TEXT TO HEX";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(803, 496);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.TexH);
+            this.Controls.Add(this.HexT);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Button HexT;
+        private System.Windows.Forms.Button TexH;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ ConvertHexToText/ConvertHexToText/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ConvertHexToText
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void HexT_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox2.Clear();
+                textBox2.Text = ConvertFromHex(textBox1.Text, Encoding.Default);
+                //textBox2.Clear();
+                //textBox1.Text = textBox1.Text.Replace(" ", "");
+                //textBox1.Text = textBox1.Text.Replace("\n\t", "");
+                //textBox1.Text = textBox1.Text.Trim();
+                //int strlen = textBox1.Text.Length;
+                //for(int i = 0; i < strlen; i = i + 2)
+                //{
+                //    string textconvert = textBox1.Text.Substring(i, 2);
+                //    int n = Convert.ToInt32(textconvert, 10);
+                //    char c = (char)n;
+                //    textBox2.Text = textBox2.Text + c.ToString();
+                //}
+            }catch(Exception ex)
+            {
+                MessageBox.Show("Conversion Error Ocurred: " + ex, "Conversion Error");
+            }
+        }
+
+        private void TexH_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                textBox1.Clear();
+                //textBox2.Text = textBox2.Text.Replace(" ", "");
+                textBox2.Text = textBox2.Text.Replace("\n\t", "");
+                textBox2.Text = textBox2.Text.Trim();
+                int strlen = textBox2.Text.Length;
+                for(int i = 0; i < strlen; i++)
+                {
+                    string hexconvert = textBox2.Text.Substring(i, 1);
+                    char c = hexconvert.ToCharArray(0, 1)[0];
+                    string n = Convert.ToString(c, 16);
+                    textBox1.Text = textBox1.Text + n;
+                }
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Conversion Error Ocurred: " + ex, "Conversion Error");
+            }
+        }
+        static string ConvertFromHex(ReadOnlySpan<char> hexString, Encoding encoding)
+        {
+            int realLength = 0;
+            for (int i = hexString.Length - 2; i >= 0; i -= 2)
+            {
+                byte b = byte.Parse(hexString.Slice(i, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+                if (b != 0) //not NULL character
+                {
+                    realLength = i + 2;
+                    break;
+                }
+            }
+
+            var bytes = new byte[realLength / 2];
+            for (var i = 0; i < bytes.Length; i++)
+            {
+                bytes[i] = byte.Parse(hexString.Slice(i * 2, 2), NumberStyles.HexNumber, CultureInfo.InvariantCulture);
+            }
+
+            return encoding.GetString(bytes);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            textBox1.Clear();
+            textBox2.Clear();
+        }
+    }
+}
